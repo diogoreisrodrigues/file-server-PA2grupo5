cc29969b1a47aa73a28751535f0d31c1974edfb3
ftferreira02 <tomasfreitas4054@gmail.com>
2023-05-01 15:03:14 +0100

    test[HMAC] : implemented the tests to HMAC

--- src/test/java/HMACTest.java
+++ src/test/java/HMACTest.java
@@ -1,23 +1,33 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class HMACTest {
-
+class HMACTest {
     @Test
     public void testComputeHMAC() throws NoSuchAlgorithmException {
-        byte[] message = "Hello World".getBytes();
-        byte[] key = "secret".getBytes();
+
+        // Input data
+        byte[] message = "test message".getBytes();
+        byte[] key = "secret key".getBytes();
         int blocksize = 64;
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-        byte[] expected = new byte[]{66, -102, 32, -74, -101, -80, 11, 7, 31, 96, 38, 58, 46, 56, 87, -124, -45, 22, 1, -118, -31, -63, 45, -116, -73, 75, 87, 58, -66, -29, 2, 28};
-        byte[] result = HMAC.computeHMAC(message, key, blocksize, messageDigest);
-        assertArrayEquals(expected, result);
+
+        // Expected output
+        byte[] expectedHMAC = new byte[] {
+                (byte) -92, (byte) 102, (byte) -89, (byte) 89, (byte) 98, (byte) -95,
+                (byte) 24, (byte) 110, (byte) -34, (byte) -102, (byte) 72, (byte) -21,
+                (byte) -50, (byte) -17, (byte) 39, (byte) 116, (byte) 47, (byte) 64, (byte) 2,
+                (byte) -99, (byte) 66, (byte) -85, (byte) -83, (byte) 84, (byte) 25, (byte) -79,
+                (byte) 88, (byte) -70, (byte) 95, (byte) -76,(byte) 84, (byte) -97
+        };
+
+        // Compute HMAC and compare with expected output
+        byte[] actualHMAC = HMAC.computeHMAC(message, key, blocksize, messageDigest);
+        Assertions.assertTrue(Arrays.equals(expectedHMAC, actualHMAC));
     }
-
-
-
 }
-
